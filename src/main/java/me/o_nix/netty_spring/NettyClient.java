efbc0272011a5f92f620c80f8b3081ae86a7c932
Kirill Vergun <code@o-nix.me>
2013-06-12 13:59:54 +0300

    Optional beans definition

--- src/main/java/me/o_nix/netty_spring/NettyClient.java
+++ src/main/java/me/o_nix/netty_spring/NettyClient.java
@@ -1,19 +1,13 @@
 package me.o_nix.netty_spring;
 
-import me.o_nix.netty_spring.initializers.NettyChannelInitializer;
-
-import org.springframework.beans.factory.InitializingBean;
-
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import me.o_nix.netty_spring.initializers.NettyChannelInitializer;
+import org.springframework.beans.factory.InitializingBean;
 
-/**
- * Author: Kirill Vergun
- * Date: 5/17/13
- */
 public class NettyClient implements InitializingBean {
 	protected NettyChannelInitializer channelInitializer;
 	protected EventLoopGroup bossGroup = new NioEventLoopGroup();
